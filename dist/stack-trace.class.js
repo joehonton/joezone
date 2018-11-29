0a7a42c17f60585c883340541d3dfa0beaf76c1b
Joe Honton <joe@joe-honton.com>
2018-11-29 14:23:38 -0800

    minify

--- dist/stack-trace.class.js
+++ dist/stack-trace.class.js
@@ -3,12 +3,12 @@ module.exports = class StackTrace {
         Object.seal(this);
     }
     static getFunctionName(e) {
-        var t = new Error().stack.split('\n')[e], a = /at (.*) ?\(/g.exec(t), r = '';
-        return null == a ? t : (a.length > 1 && (r += a[1].trim()), `{${r = r.padStart(30, ' ')}}`);
+        var t = new Error().stack.split('\n')[e], a = /at (.*) ?\(/g, r = a.exec(t), n = '';
+        return null == r ? t : (r.length > 1 && (n += r[1].trim()), `{${n = n.padStart(30, ' ')}}`);
     }
     static getSitus(e) {
-        var t = new Error().stack.split('\n')[e], a = /at .*\((.*)\)/g.exec(t), r = '';
-        return a.length > 1 && (r += a[1].trim()), r;
+        var t = new Error().stack.split('\n')[e], a = /at .*\((.*)\)/g, r = a.exec(t), n = '';
+        return r.length > 1 && (n += r[1].trim()), n;
     }
     static getInfo(e) {
         var t = {
@@ -18,16 +18,16 @@ module.exports = class StackTrace {
             filename: '',
             line: '',
             column: ''
-        }, a = new Error().stack.split('\n')[e], r = /at (.*) ?\(/g.exec(a), n = '';
-        r.length > 1 && (n = r[1].trim());
-        var l = n.split('.');
-        t.classname = l[0], l.length > 1 && (t.member = l[1], t.member = t.member.replace(' (eval at evaluate', ''));
-        var s = /at .*\((.*)\)/g.exec(a), c = '';
-        s.length > 1 && (c = s[1].trim());
-        var m = c.split(':'), i = m[0];
-        m.length > 1 && (t.line = m[1]), m.length > 2 && (t.column = m[2]);
-        var g = i.lastIndexOf('/');
-        return -1 != g ? (t.path = i.substr(0, g), t.filename = i.substr(g + 1)) : t.filename = i, 
+        }, a = new Error().stack.split('\n')[e], r = /at (.*) ?\(/g, n = r.exec(a), l = '';
+        n.length > 1 && (l = n[1].trim());
+        var s = l.split('.');
+        t.classname = s[0], s.length > 1 && (t.member = s[1], t.member = t.member.replace(' (eval at evaluate', ''));
+        var c = /at .*\((.*)\)/g, m = c.exec(a), i = '';
+        m.length > 1 && (i = m[1].trim());
+        var g = i.split(':'), u = g[0];
+        g.length > 1 && (t.line = g[1]), g.length > 2 && (t.column = g[2]);
+        var o = u.lastIndexOf('/');
+        return -1 != o ? (t.path = u.substr(0, o), t.filename = u.substr(o + 1)) : t.filename = u, 
         t;
     }
 };
